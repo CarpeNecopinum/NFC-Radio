a2137545e77475d10cfe6d2990ad8cdb0db9edfb
CarpeNecopinum <CarpeNecopinum@users.noreply.github.com>
2021-05-12 21:00:40 +0200

    Reconnect to MPD when commands fail

--- src/MPDConnection.hh
+++ src/MPDConnection.hh
@@ -6,11 +6,7 @@ class MPDConnection
 public:
     MPDConnection()
     {
-        mConn = mpd_connection_new(nullptr, 0, 0);
-        if (mpd_connection_get_error(mConn) != MPD_ERROR_SUCCESS)
-        {
-            throw new std::runtime_error(mpd_connection_get_error_message(mConn));
-        }
+        reconnect();
     }
 
     ~MPDConnection()
@@ -21,23 +17,54 @@ public:
 
     bool add(std::string const &url)
     {
-        auto res = mpd_run_add(mConn, url.c_str());
-        if (!res)
-            std::cerr << "Could not add " << url << "\n"
-                      << mpd_connection_get_error_message(mConn) << std::endl;
-        return res;
+        return retry([&]() {
+            auto res = mpd_run_add(mConn, url.c_str());
+            if (!res)
+                std::cerr << "Could not add " << url << "\n"
+                          << mpd_connection_get_error_message(mConn) << std::endl;
+            return res;
+        });
     }
 
     bool clear()
     {
-        return mpd_run_clear(mConn);
+        return retry([&]() {
+            return mpd_run_clear(mConn);
+        });
     }
 
     bool play()
     {
-        return mpd_run_play(mConn);
+        return retry([&]() {
+            return mpd_run_play(mConn);
+        });
     }
 
 protected:
-    mpd_connection *mConn;
+    mpd_connection *mConn = nullptr;
+    void reconnect()
+    {
+        if (mConn)
+            mpd_connection_free(mConn);
+        mConn = mpd_connection_new(nullptr, 0, 0);
+        if (mpd_connection_get_error(mConn) != MPD_ERROR_SUCCESS)
+        {
+            throw new std::runtime_error(mpd_connection_get_error_message(mConn));
+        }
+        mpd_connection_set_keepalive(mConn, true);
+    }
+
+    template <class FN, typename... Args>
+    bool retry(FN &&fn, Args &&...args)
+    {
+        int tries = 3;
+        do
+        {
+            auto success = fn(std::forward<Args>(args...)...);
+            if (success)
+                return true;
+            reconnect();
+        } while (tries--);
+        return false;
+    }
 };
